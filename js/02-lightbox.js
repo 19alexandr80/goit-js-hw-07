690129cfad44013d7a1257b79da9efb9f02ba546
19alexandr80 <usanka1980@gmail.com>
2023-02-27 05:56:59 +0200

    li_add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,9 +6,9 @@ console.log(galleryItems);
 const galleryEl = document.querySelector(".gallery");
 const createGallery = galleryItems
   .map(({ preview, original, description }) => {
-    return `<a class="gallery__item" href="${original}">
+    return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
   })
   .join("");
 galleryEl.insertAdjacentHTML("beforeEnd", createGallery);
